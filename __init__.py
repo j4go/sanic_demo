74557466e7692c8cf9088322d58fe3da5e3cda8c
lgf17 <yianny@163.com>
2018-01-11 17:29:42 +0800

    sanic路由的组织学习

---
+++ __init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import logging
+from sanic import Sanic
+# import config
+
+app = Sanic()
+
+# app.config.from_object(config)
+
+# logging.basicConfig(format=app.config.get('LOG_FORMAT'))
+# logging.getLogger().setLevel(app.config.get('LOG_LEVEL'))
+#
+# log = logging
